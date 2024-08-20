e6b79bfeea578451ec817af3624a136a3db3a934
alergio <101852890+alergio@users.noreply.github.com>
2024-08-20 19:33:27 -0300

    [Logout] (#12)
    * [Logout]
    
    * Ajuste post-merge conflicts

--- src/auth/router.mjs
+++ src/auth/router.mjs
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { login, verifyToken, signUp } from "./service.mjs";
 import { checkRole } from "../admin/service.mjs";
+import { cookies } from "./constants.mjs";
 
 export const authRouter = Router();
 
@@ -23,7 +24,7 @@ authRouter.post("/login", async (req, res) => {
     };
 
     // Configuro la cookie del token
-    res.cookie("token", sessionData.token, cookieConfig);
+    res.cookie(cookies.token.name, sessionData.token, cookieConfig);
 
     // Envio la info del usuario
     res.json({ user: sessionData.user });
@@ -32,15 +33,12 @@ authRouter.post("/login", async (req, res) => {
     res.status(401).json({ error: error.message });
   }
 });
-// Ruta protegida de ejemplo
-authRouter.get(
-  "/protected",
-  verifyToken,
-  checkRole(["ADMIN"]),
-  async (req, res) => {
-    res.json({ message: "Access granted", user: req.user });
-  }
-);
+
+authRouter.get("/logout", verifyToken, async (req, res) => {
+  res.clearCookie(cookies.token.name);
+  return res.redirect("/");
+});
+
 authRouter.post("/sign-up", async (req, res, next) => {
   const { name, lastName, email, password, phone } = req.body;
   if (!name || !/^[a-zA-Z]+(\s[a-zA-Z]+)*$/.test(name)) {
@@ -91,3 +89,13 @@ authRouter.post("/sign-up", async (req, res, next) => {
     next(error);
   }
 });
+
+// Ruta protegida de ejemplo
+authRouter.get(
+  "/protected",
+  verifyToken,
+  checkRole(["ADMIN"]),
+  async (req, res) => {
+    res.json({ message: "Access granted", user: req.user });
+  }
+);
