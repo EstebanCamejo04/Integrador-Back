9cffdcd8fcf885436694b2fbe264afb70856d703
Bautisc <84480374+Bautisc@users.noreply.github.com>
2024-09-01 15:01:22 -0300

    Feature/product stuff (#18)
    * More and more schemas yay🔧💩
    
    * More and more schemas yay🔧💩
    
    * PR de emergencias
    
    * PR de emergencias

--- src/products/service.mjs
+++ src/products/service.mjs
@@ -1,16 +1,65 @@
+import { S3Client, PutObjectCommand, GetObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
 import { prisma } from "../db.mjs";
+
+import dotenv from 'dotenv';
+import multer from "multer";
+
+dotenv.config()
+
+// S3 Credentials
+const bucket_name = process.env.BUCKET_NAME 
+const bucket_region = process.env.BUCKET_REGION 
+const access_key = process.env.ACCESS_KEY 
+const secret_access_key = process.env.SECRET_ACCESS_KEY 
+
+const s3 = new S3Client({
+    credentials: {
+        accessKeyId: access_key,
+        secretAccessKey: secret_access_key,
+    },
+    region:  bucket_region
+})
+
+// Multer config for handling single Image
+export const multerHandler = ()=> {
+    const storage = multer.memoryStorage();
+
+    console.log("paso por el multer")
+    return multer({ storage }).single("image_url");
+}
 
 
 // PRODUCTS 
 //
+// Create a new product
+export const postProduct = async ({ name, description, categoryId, price, available}) => {
+    try {
+        
+        const newProduct = await prisma.product.create({
+            data: {
+                name,
+                description,
+                category: {
+                    connect: { id: categoryId } 
+                },
+                price,
+                available,
+            }
+        });
+        return newProduct;
+    } catch (error) {
+        console.error("Error creating product:", error);
+        throw new Error("Unable to create product. Please try again later.");
+    }
+};
 // Fetch all products with related data
 export const getAllProducts = async () => {
     try {
         return prisma.product.findMany({
             include: {
                 category: true,
-                productDate: true,
-                location: true
+                product_date: true,
+                product_location: true
             }
         });
     } catch (error) {
@@ -28,7 +77,7 @@ export const productById = async (id) => {
             },
             include: {
                 category: true,
-                productDate: true,
+                product_date: true,
                 product_feature: { // Tabla Intermedia de caracteristicas
                     include: {
                         feature: true //Incluir detalles de caracteristicas 
@@ -42,17 +91,6 @@ export const productById = async (id) => {
     }
 };
 
-// Create a new product
-export const postProduct = async (productData) => {
-    try {
-        return prisma.product.create({
-            data: productData
-        });
-    } catch (error) {
-        console.error("Error creating product:", error);
-        throw new Error("Unable to create product. Please try again later.");
-    }
-};
 
 // PRODUCTS TYPES
 export const getProductCategories = async () =>{
