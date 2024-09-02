97e6af69da15327767dad9f8272fcfcd5fdca502
Ana Hurtado <anahurtado0104@gmail.com>
2024-09-02 13:34:41 -0500

    backend busqueda de producto

--- src/products/router.mjs
+++ src/products/router.mjs
@@ -1,99 +1,141 @@
 import { Router } from "express";
-import { getAllProdFeatures, getAllProducts, getProductCategories, getProductDates, postProduct, postProductCategorie, postProductDate, productById } from "./service.mjs"
+import {
+  getAllProdFeatures,
+  getAllProducts,
+  getProductCategories,
+  getProductDates,
+  postProduct,
+  postProductCategorie,
+  postProductDate,
+  productById,
+  searchProducts,
+} from "./service.mjs";
 
 export const productRouter = Router();
 
-//  ENDPOINTS FOR PRODUCTS 
+//  ENDPOINTS FOR PRODUCTS
 
 // GET products
 productRouter.get("/products", async (req, res) => {
-    try {
-        const products = await getAllProducts();
-        res.status(200).json(products);
-    } catch (error) {
-        res.status(500).json({message:"Failed to fetch all products", error: error.message})
-    }
+  try {
+    const products = await getAllProducts();
+    res.status(200).json(products);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Failed to fetch all products", error: error.message });
+  }
 });
 
 // GET products by ID
 productRouter.get("/products/:id", async (req, res) => {
-    try {
-        const id = req.params.id;
+  try {
+    const id = req.params.id;
 
-        const findProdByID = await productById(id);
-        res.status(200).json(findProdByID);
-    } catch (error) {
-        res.status(500).json({message:"Failed to fetch all products", error: error.message})
-    }
+    const findProdByID = await productById(id);
+    res.status(200).json(findProdByID);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Failed to fetch all products", error: error.message });
+  }
 });
 
 // POST products
 productRouter.post("/products", async (req, res) => {
-    try {
-
-        const newProduct = await postProduct(req.body);
-        res.status(201).json(newProduct);
-    } catch (error) {
-        res.status(500).json({message:"Failed to fetch all products", error: error.message})
-    }
+  try {
+    const newProduct = await postProduct(req.body);
+    res.status(201).json(newProduct);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Failed to fetch all products", error: error.message });
+  }
 });
+// POST search-products
+productRouter.get("/search-products", async (req, res) => {
+  const words = req.query.words;
+  const start = req.query.start;
+  const end = req.query.end;
+  if (!words && !start && !end) {
+    return res.status(400).json({
+      error:
+        "Debes buscar por al menos un criterio (rango de fechas o palabras)",
+    });
+  }
 
+  try {
+    const newProduct = await searchProducts(words, start, end);
+    res.status(200).json(newProduct);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Failed to searching products", error: error.message });
+  }
+});
 // ENDPOINTS FOR Categories
 
 // GET products categories
 productRouter.get("/products_categories", async (req, res) => {
-    try {
-        const getCategories = await getProductCategories();
-        res.status(200).json(getCategories);
-    } catch (error) {
-        res.status(500).json({message:"Failed to fetch all products categories", error: error.message})
-    }
+  try {
+    const getCategories = await getProductCategories();
+    res.status(200).json(getCategories);
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to fetch all products categories",
+      error: error.message,
+    });
+  }
 });
 
 // POST product categories
 productRouter.post("/products_categories", async (req, res) => {
-    try {
-
-        const postCategories  = await postProductCategorie(req.body);
-        res.status(201).json(postCategories);
-    } catch (error) {
-
-        res.status(500).json({message:"Failed to fetch all products categories", error: error.message})
-    }
+  try {
+    const postCategories = await postProductCategorie(req.body);
+    res.status(201).json(postCategories);
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to fetch all products categories",
+      error: error.message,
+    });
+  }
 });
 
 // ENDPOINTS FOR PRODUCTS DATES
 
 // GET products dates
 productRouter.get("/products_dates", async (req, res) => {
-    try {
-        const getDates = await getProductDates();
-        res.status(200).json(getDates);
-    } catch (error) {
-        res.status(500).json({message:"Failed to fetch all products dates", error: error.message})
-    }
+  try {
+    const getDates = await getProductDates();
+    res.status(200).json(getDates);
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to fetch all products dates",
+      error: error.message,
+    });
+  }
 });
 
 // POST product dates
 productRouter.post("/products_dates", async (req, res) => {
-    try {
-        const postDates = await postProductDate(req.body);      
-        res.status(201).json(postDates);
-    } catch (error) {
-        res.status(500).json({message:"Failed to post products dates", error: error.message})
-
-    }
+  try {
+    const postDates = await postProductDate(req.body);
+    res.status(201).json(postDates);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Failed to post products dates", error: error.message });
+  }
 });
 
 // ENDPOINTS FOR FEATURES
-productRouter.get("/products_features", async (req,res) => {
-    try {
-        const productFeatures = await getAllProdFeatures();
-        res.status(200).json(productFeatures);
-    } catch (error) {
-        res.status(500).json({message:"Failed to fetch all features", error: error.message})
-    }
-
-})
-
-
+productRouter.get("/products_features", async (req, res) => {
+  try {
+    const productFeatures = await getAllProdFeatures();
+    res.status(200).json(productFeatures);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Failed to fetch all features", error: error.message });
+  }
+});
--- src/products/service.mjs
+++ src/products/service.mjs
@@ -1,123 +1,180 @@
 import { prisma } from "../db.mjs";
 
-
-// PRODUCTS 
+// PRODUCTS
 //
 // Fetch all products with related data
 export const getAllProducts = async () => {
-    try {
-        return prisma.product.findMany({
-            include: {
-                category: true,
-                productDate: true,
-                location: true
-            }
-        });
-    } catch (error) {
-        console.error("Error fetching all products:", error);
-        throw new Error("Unable to fetch products. Please try again later.");
-    }
+  try {
+    return prisma.product.findMany({
+      include: {
+        category: true,
+        productDate: true,
+        location: true,
+      },
+    });
+  } catch (error) {
+    console.error("Error fetching all products:", error);
+    throw new Error("Unable to fetch products. Please try again later.");
+  }
+};
+export const searchProducts = async (words, start, end) => {
+  try {
+    return prisma.product.findMany({
+      where: {
+        AND: [
+          {
+            OR: [
+              {
+                name: {
+                  contains: words,
+                },
+              },
+              {
+                description: {
+                  contains: words,
+                },
+              },
+              {
+                category: {
+                  name: {
+                    contains: words,
+                  },
+                },
+              },
+              {
+                location: {
+                  name: {
+                    contains: words,
+                  },
+                },
+              },
+            ],
+          },
+          start && end
+            ? {
+                productDate: {
+                  date: {
+                    gte: start,
+                    lte: end,
+                  },
+                },
+              }
+            : {},
+        ],
+      },
+      include: {
+        category: true,
+        productDate: true,
+        location: true,
+      },
+    });
+  } catch (error) {
+    console.error("Error fetching all products:", error);
+    throw new Error("Unable to fetch products. Please try again later.");
+  }
 };
 
 // Fetch a product by ID with related data
 export const productById = async (id) => {
-    try {
-        return prisma.product.findFirst({
-            where: {
-                id: parseInt(id, 10)
-            },
-            include: {
-                category: true,
-                productDate: true,
-                product_feature: { // Tabla Intermedia de caracteristicas
-                    include: {
-                        feature: true //Incluir detalles de caracteristicas 
-                    }
-                }
-            }
-        });
-    } catch (error) {
-        console.error("Error fetching product by ID:", error);
-        throw new Error("Unable to fetch product details. Please try again later.");
-    }
+  try {
+    return prisma.product.findFirst({
+      where: {
+        id: parseInt(id, 10),
+      },
+      include: {
+        category: true,
+        productDate: true,
+        product_feature: {
+          // Tabla Intermedia de caracteristicas
+          include: {
+            feature: true, //Incluir detalles de caracteristicas
+          },
+        },
+      },
+    });
+  } catch (error) {
+    console.error("Error fetching product by ID:", error);
+    throw new Error("Unable to fetch product details. Please try again later.");
+  }
 };
 
 // Create a new product
 export const postProduct = async (productData) => {
-    try {
-        return prisma.product.create({
-            data: productData
-        });
-    } catch (error) {
-        console.error("Error creating product:", error);
-        throw new Error("Unable to create product. Please try again later.");
-    }
+  try {
+    return prisma.product.create({
+      data: productData,
+    });
+  } catch (error) {
+    console.error("Error creating product:", error);
+    throw new Error("Unable to create product. Please try again later.");
+  }
 };
 
 // PRODUCTS TYPES
-export const getProductCategories = async () =>{
-
-    try {
-        return await prisma.product_category.findMany({
-            include: {
-                products: true,
-            },
-        });
-
-    } catch (error) {
-        console.error("Error fetching product categories:", error);
-        throw new Error("Unable to fetch product categories. Please try again later.");
-
-    }
-}
+export const getProductCategories = async () => {
+  try {
+    return await prisma.product_category.findMany({
+      include: {
+        products: true,
+      },
+    });
+  } catch (error) {
+    console.error("Error fetching product categories:", error);
+    throw new Error(
+      "Unable to fetch product categories. Please try again later."
+    );
+  }
+};
 
 export const postProductCategorie = async (req, res) => {
-
-    try {
-
-        return await prisma.product_category.create({
-            data: req.body,
-        });
-    } catch (error) {
-        console.error("Error posting product categories:", error);
-        throw new Error("Unable to fetch product categories. Please try again later.");
-    }
-}
+  try {
+    return await prisma.product_category.create({
+      data: req.body,
+    });
+  } catch (error) {
+    console.error("Error posting product categories:", error);
+    throw new Error(
+      "Unable to fetch product categories. Please try again later."
+    );
+  }
+};
 // PRODUCT DATES
-export const getProductDates =async () => {
-    try {
+export const getProductDates = async () => {
+  try {
     return await prisma.product_date.findMany({
-        include: {
-            products: true,
-        },
+      include: {
+        products: true,
+      },
     });
-    } catch (error) {
-        console.error("Error fetching product dates:", error);
-        throw new Error("Unable to fetch product dates. Please try again later.");
-    }
-}
+  } catch (error) {
+    console.error("Error fetching product dates:", error);
+    throw new Error("Unable to fetch product dates. Please try again later.");
+  }
+};
 
-export const postProductDate = async (req, res) =>  {
-    return await prisma.product_date.create({
-        data: req.body,
-    });
-}
+export const postProductDate = async (req, res) => {
+  return await prisma.product_date.create({
+    data: req.body,
+  });
+};
 // PRODUCT FEATURES
 //
 // Fetch all product features with related data
 export const getAllProdFeatures = async () => {
-    try {
-        return prisma.feature.findMany({
-            include: {
-                product_feature: {
-                    include: {
-                        product: true
-                    }
-                }
-            }
-        });
-    } catch (error) {
-        console.error("Error fetching all product features:", error);
-        throw new Error("Unable to fetch product features. Please try again later.");
-    }
+  try {
+    return prisma.feature.findMany({
+      include: {
+        product_feature: {
+          include: {
+            product: true,
+          },
+        },
+      },
+    });
+  } catch (error) {
+    console.error("Error fetching all product features:", error);
+    throw new Error(
+      "Unable to fetch product features. Please try again later."
+    );
+  }
 };
