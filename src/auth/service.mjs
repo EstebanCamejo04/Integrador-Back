e9ab4f106436f0e7b0b6d4cf26da9f9ab81d4c27
alergio <101852890+alergio@users.noreply.github.com>
2024-09-18 20:04:54 -0300

    [API] Save token out of session (#26)
    * [API] Save token out of session
    
    * ajuste

--- src/auth/service.mjs
+++ src/auth/service.mjs
@@ -53,20 +53,34 @@ export const signUp = async (name, lastName, email, password, phone) => {
 // Middleware para verificar el token
 export const verifyToken = async (req, res, next) => {
   // Obtengo el token de las cookies
-  const authToken = req.cookies.token;
+  let token;
 
-  // Valido que se haya obtenido un token
-  if (!authToken) {
-    return res.status(401).json({ error: "Access denied. No token provided." });
+  token = req.cookies.token;
+
+  // Valido que obtuve un token
+  if (!token) {
+    token = req.headers["authorization"]
+      ?.split(" ")[1]
+      .replace(/^"|"$/g, "")
+      .trim();
+
+    if (!token) {
+      return res
+        .status(401)
+        .json({ message: "Access denied. No token provided." });
+    }
   }
+
+  console.log(token);
 
   // const token = authToken.split(" ")[1]; // Extract token from "Bearer <token>"
 
   try {
-    const decoded = jwt.verify(authToken, SECRET_KEY);
+    const decoded = jwt.verify(token, SECRET_KEY);
     req.user = decoded; // Añadir los datos del usuario al request
     next(); // Continuar con la siguiente función en la cadena
   } catch (error) {
+    console.log(error);
     return res.status(401).json({ error: "Invalid token" });
   }
 };
